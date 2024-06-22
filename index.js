290f7215f21d3d2696356e186c8a72d1d2dffc87
PabloGiordani <pablo.giordani@loft.com.br>
2024-06-22 12:25:33 -0300

    Testando conectivadade com OPENAI

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const OpenAI = require('openai');
+
+//dotenv
+const dotenv = require('dotenv') 
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const openai = new OpenAI(
+    {
+        apiKey: process.env.OPENAI_API_KEY, 
+    }
+);
+
+app.get('/', async (req, res) => {
+    let content = await main();
+    res.send(content);
+});
+
+let port =  process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+async function main() {
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: "Pode me dizer uma frase motivacional?" }
+      ],
+      model: "gpt-4o",
+    });
+  
+    return completion.choices[0].message.content;
+  }
+  
+
+
